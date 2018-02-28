a9a523493d9d9aeb75d6f3b194f09e61eb20e512
meninosousa <meninosousa@gmail.com>
2018-02-28 20:37:28 +0100

    test

--- Program/naspi.py
+++ Program/naspi.py
@@ -110,7 +110,7 @@ def main():
             elif switch == 1:
                 if getSwitch() == switch:
                     print('option 1')
-#TESTTTTT
+#TESTTTTT2
                 onYellow()
             elif switch == 2:
                 if getSwitch() == switch:
